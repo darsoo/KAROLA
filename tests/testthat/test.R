9e19ac24b916cd7a4a443aed53cd7de3327388d3
darsoo <daro.scig@gmail.com>
2017-09-22 00:02:53 +0200

    change format of input database info to json, and add more accurate dataframes tests.

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,32 +1,16 @@
-dbname <- readline(prompt = "write database name: ")
-port <- try(as.numeric(readline(prompt = "write postgres port: ")))
-host <- readline(prompt = "write postgres host: " )
-user <- readline(prompt = "write postgres username: ")
-password <- readline(prompt = "write password for user: ")
-
-# dbname <- "postgres"
-# port <- 5432
-# host <- "localhost"
-# user <- "daroso"
-# password <- "xxxxxx"
-
 ##############################################################################################################
 # test_create_database.R
 library(KAROLA)
 context("create_database")
 
-drop_test_data_base(dbname = dbname, host = host, port = port, user = user, password = password)
-create_test_data_base(dbname = dbname, host = host, port = port, user = user, password = password)
+#drop_test_data_base(json = "test.json")
+create_test_data_base(json = "test.json")
 
 #####
 arguments <- list(
     word = "cancer_NN",
     lemma_table = "test_new_table_1",
-    dbname = "test_data_base",
-    host = host,
-    port = port,
-    user = user,
-    password = password
+    json = "test.json"
 )
 test_that("make table with good word",{
     expect_null(do.call(create_database, arguments))
@@ -35,42 +19,42 @@ test_that("make table with good word",{
 arguments <- list(
     word = "wrong_word",
     lemma_table = "test_new_table_2",
-    dbname = "test_data_base",
-    host = "localhost",
-    port = 5432,
-    user = user,
-    password = password
+    json = "test.json"
 )
 test_that("make table with wrong word",{
-    expect_error(do.call(create_database, arguments))
+    expect_error(do.call(create_database, arguments),paste0('given word: "' ,arguments$word ,'" does not exist in database'))
 })
 #####
-drop_test_data_base(dbname = dbname, host = host, port = port, user = user, password = password)
+drop_test_data_base(json = "test.json")
 ##############################################################################################################
 # test_create_table_with_count_words.R
 
 library(KAROLA)
 context("create_table_with_count_words")
 
-create_test_data_base(dbname = dbname, host = host, port = port, user = user, password = password)
+create_test_data_base(json = "test.json")
 
 #####
+arguments <- list(word = "cancer_NN",
+                  lemma_table = "test_table",
+                  json = "test.json")
+
+do.call(create_database, arguments)
+#####
+
 arguments <- list(lemma_table = "test_table",
-                  dbname = "test_data_base",
-                  host = "localhost",
-                  port = 5432,
-                  user = user,
-                  password = password,
+                  json = "test.json",
                   first_analyzed_date = "2016-01-01",
                   last_analyzed_date = "2016-12-01")
 
-do.call(create_database, c("cancer_NN",arguments[c("lemma_table","dbname","host","port","user","password")]))
-#####
+
 function_output <- do.call(create_table_with_count_words, arguments)
 
 test_that("background info", {
     expect_equal(function_output[2,1], "0")
     expect_equal(function_output[2,2], 175)
+    expect_equal(nrow(function_output), 2057)
+    expect_equal(ncol(function_output), 2)
 })
 #####
 use_argument <- arguments
@@ -81,6 +65,8 @@ function_output <- do.call(create_table_with_count_words, use_argument)
 
 test_that("test for fix bug - 1", {
     expect_is(function_output, "data.frame")
+    expect_equal(nrow(function_output), 915)
+    expect_equal(ncol(function_output), 3)
 })
 #####
 use_argument <- arguments
@@ -91,8 +77,10 @@ function_output <- do.call(create_table_with_count_words, use_argument)
 
 test_that("test for fix bug - 2", {
     expect_is(function_output, "data.frame")
+    expect_equal(nrow(function_output), 1043)
+    expect_equal(ncol(function_output), 3)
 })
 #####
-drop_test_data_base(dbname = dbname, host = host, port = port, user = user, password = password)
+drop_test_data_base(json = "test.json")
 
 ##############################################################################################################

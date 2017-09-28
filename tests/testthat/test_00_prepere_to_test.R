445add013689d8ed4452cc8f8958d2e81f06418c
darsoo <daro.scig@gmail.com>
2017-09-28 23:48:08 +0200

    separate file which prepare environment for tests

---
+++ tests/testthat/test_00_prepere_to_test.R
@@ -0,0 +1,24 @@
+##############################################################################################################
+# test_00_prepere_to_test.R
+library(KAROLA)
+context("prepere_to_test")
+
+KAROLA_container_id <- system("docker ps -q -f name=KarolaTestDatabase", intern = T)
+if (length(KAROLA_container_id) > 0){
+    try(system("docker stop KarolaTestDatabase"))
+    try(system("docker rm KarolaTestDatabase"))
+}
+
+system("docker run --name KarolaTestDatabase -p 5430:5432 -d daroso/karola-test-database:0.9002")
+
+log_from_karola_docker <- ''
+time_from_ran <- 0
+print("Please wait a moment, loading postgres database.")
+while(log_from_karola_docker[length(log_from_karola_docker)] != "LOG:  autovacuum launcher started"){
+    time_from_ran <- time_from_ran + 1
+    print(paste0("Proces starts ",time_from_ran," second ago."))
+    log_from_karola_docker <- system2("docker","logs KarolaTestDatabase",stdout = TRUE,stderr = TRUE)
+    Sys.sleep(1)
+}
+print("Postgres database was loaded.")
+Sys.sleep(3)
